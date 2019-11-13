bd8ecd6b60019149a846282280a07a113e04a732
Eric Dam <dam.eric3@gmail.com>
2019-11-13 10:43:40 -0800

    updated scripts

--- script/create_user.js
+++ script/create_user.js
@@ -7,15 +7,11 @@ function createUser() {
         db.collection("users").doc(user.uid).set({
             "name":user.displayName, 
             "email":user.email,
-            "total": 0,
         });
         db.collection("users").doc(user.uid).collection("tasks").doc("task1").set({
             "Name": "Bob",
+            "Category": "Birthday",
             "Due_Date": "January 19",
-        });
-        db.collection("users").doc(user.uid).collection("tasks").doc("task2").set({
-            "Name": "Dylan",
-            "Due_Date": "December 25",
         });
     });
 }
--- script/load_tasks.js
+++ script/load_tasks.js
@@ -6,8 +6,9 @@ function loadTasks() {
                 listItem.className = "list-group-item";
                 console.log(doc.data());
                 var name = doc.data().Name;
+                var category = doc.data().Category;
                 var dueDate = doc.data().Due_Date;
-                listItem.innerHTML = "Name: " + name + "<br>" + "Due Date: " + dueDate;
+                listItem.innerHTML = "Name: " + name + "<br>" +"Category: " + category + "<br>" + "Due Date: " + dueDate;
                 var ele = document.getElementsByClassName("list-group");
                 for (var x = 0; x < ele.length; x++) {
                     ele[x].append(listItem);
