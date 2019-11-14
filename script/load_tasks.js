f826b261f0515a2dfd3fc03a2a78879ff49f7a79
Eric Dam <dam.eric3@gmail.com>
2019-11-14 15:27:10 -0800

    Allows removal of tasks

--- script/load_tasks.js
+++ script/load_tasks.js
@@ -1,17 +1,75 @@
 function loadTasks() {
-    firebase.auth().onAuthStateChanged(function (user) {
-        db.collection("users").doc(user.uid).collection("tasks").onSnapshot(function (querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                var listItem = document.createElement("LI");
-                listItem.className = "list-group-item";
-                console.log(doc.data());
-                var name = doc.data().Name;
-                var category = doc.data().Category;
-                var dueDate = doc.data().Due_Date;
-                listItem.innerHTML = "Name: " + name + "<br>" +"Category: " + category + "<br>" + "Due Date: " + dueDate;
-                var ele = document.getElementsByClassName("list-group");
-                for (var x = 0; x < ele.length; x++) {
-                    ele[x].append(listItem);
+    firebase.auth().onAuthStateChanged(function(user) {
+        db.collection("users").doc(user.uid).collection("tasks").onSnapshot(function(querySnapshot) {
+            querySnapshot.docChanges().forEach(function(change) {
+                if (change.type == "added") {
+                    var docName = change.doc.id;
+                    var listItem = document.createElement("LI");
+                    var button = document.createElement("BUTTON");
+                    listItem.className = "list-group-item";
+                    button.className = "btn btn-outline-danger";
+                    console.log(change.doc.data());
+                    var name = change.doc.data().Name;
+                    var category = change.doc.data().Category;
+                    var dueDate = change.doc.data().Due_Date;
+                    listItem.innerHTML = "Name: " + name + "<br>" +"Category: " + category + "<br>" + "Due Date: " + dueDate;
+                    button.innerHTML = "Delete";
+                    $(button).css("display", "none");
+                    $(button).on("click", function() {
+                        $(this).parent().remove();
+                        db.collection("users").doc(user.uid).collection("tasks").doc(docName).delete();
+                    });
+                    listItem.append(button);
+                    var clicked = false;
+                    $(listItem).on("click", function() {
+                        if (!clicked) {
+                            $(button).css("display","block");
+                            clicked = true;
+                        }
+                        else{
+                            $(button).css("display","none");
+                            clicked = false;
+                        }
+                    });
+                    var ele = document.getElementsByClassName("list-group");
+                    for (var x = 0; x < ele.length; x++) {
+                        ele[x].append(listItem);
+                    }
+                }
+                if (change.type == "modified") {
+                    var docName = change.doc.id;
+                    var listItem = document.createElement("LI");
+                    var button = document.createElement("BUTTON");
+                    listItem.className = "list-group-item";
+                    button.className = "btn btn-outline-danger";
+                    console.log(change.doc.data());
+                    var name = change.doc.data().Name;
+                    var category = change.doc.data().Category;
+                    var dueDate = change.doc.data().Due_Date;
+                    listItem.innerHTML = "Name: " + name + "<br>" +"Category: " + category + "<br>" + "Due Date: " + dueDate;
+                    button.innerHTML = "Delete";
+                    $(button).css("display", "none");
+                    $(button).on("click", function() {
+                        $(this).parent().remove();
+                        db.collection("users").doc(user.uid).collection("tasks").doc(docName).delete();
+                    });
+                    listItem.append(button);
+                    var clicked = false;
+                    $(listItem).on("click", function() {
+                        if (!clicked) {
+                            $(button).css("display","block");
+                            clicked = true;
+                        }
+                        else{
+                            $(button).css("display","none");
+                            clicked = false;
+                        }
+                    });
+                    var ele = document.getElementsByClassName("list-group");
+                    ele[ele.length-1].append(listItem);
+                }
+                if (change.type == "removed") {
+                    console.log("Document Removed");
                 }
             });
         });
