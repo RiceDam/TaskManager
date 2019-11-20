af43937eae502b23ec4e78f5faa402d64dfbd530
Eric Dam <dam.eric3@gmail.com>
2019-11-19 16:52:27 -0800

    Added timestamps

--- script/add_task.js
+++ script/add_task.js
@@ -1,10 +1,11 @@
-function addTask(name, category, due, task) {
+function addTask(name, category, due) {
     firebase.auth().onAuthStateChanged(function (user) {
         db.collection("users").doc(user.uid).collection("tasks").get().then(function (querySnapshot) {
             db.collection("users").doc(user.uid).collection("tasks").doc().set({
                 "Name": name,
                 "Category": category,
                 "Due_Date": due,
+                "Time_Stamp": new Date(),
             });
         });
     });
--- script/load_tasks.js
+++ script/load_tasks.js
@@ -1,6 +1,6 @@
 function loadTasks() {
     firebase.auth().onAuthStateChanged(function(user) {
-        db.collection("users").doc(user.uid).collection("tasks").onSnapshot(function(querySnapshot) {
+        db.collection("users").doc(user.uid).collection("tasks").orderBy("Time_Stamp").onSnapshot(function(querySnapshot) {
             querySnapshot.docChanges().forEach(function(change) {
                 if (change.type == "added") {
                     var docName = change.doc.id;
@@ -12,7 +12,8 @@ function loadTasks() {
                     var name = change.doc.data().Name;
                     var category = change.doc.data().Category;
                     var dueDate = change.doc.data().Due_Date;
-                    listItem.innerHTML = "Name: " + name + "<br>" +"Category: " + category + "<br>" + "Due Date: " + dueDate;
+                    var time = change.doc.data().Time_Stamp;
+                    listItem.innerHTML = "Name: " + name + "<br>" +"Category: " + category + "<br>" + "Due Date: " + dueDate + "<br>" + "Time Stamp: " + time.toDate();
                     button.innerHTML = "Delete";
                     $(button).css("display", "none");
                     $(button).on("click", function() {
@@ -46,7 +47,8 @@ function loadTasks() {
                     var name = change.doc.data().Name;
                     var category = change.doc.data().Category;
                     var dueDate = change.doc.data().Due_Date;
-                    listItem.innerHTML = "Name: " + name + "<br>" +"Category: " + category + "<br>" + "Due Date: " + dueDate;
+                    var time = change.doc.data().Time_Stamp;
+                    listItem.innerHTML = "Name: " + name + "<br>" +"Category: " + category + "<br>" + "Due Date: " + dueDate + "<br>" + "Time Stamp: " + time.toDate();
                     button.innerHTML = "Delete";
                     $(button).css("display", "none");
                     $(button).on("click", function() {
