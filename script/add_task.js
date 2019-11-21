d843e69524af658a56aebde015b15a5cf9814308
Jeffrey To <jleeto93@gmail.com>
2019-11-20 19:29:12 -0800

    A few comments

--- script/add_task.js
+++ script/add_task.js
@@ -1,3 +1,4 @@
+// Create Function to add task to database
 function addTask(name, category, due) {
     firebase.auth().onAuthStateChanged(function (user) {
         db.collection("users").doc(user.uid).collection("tasks").get().then(function (querySnapshot) {
